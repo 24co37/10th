a952c0d89c72fcc4ebe90c19a0dfa9517a3059d6
24co37 <24co37@aitdgoa.edu.in>
2026-05-26 11:18:43 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.io.FileWriter; 
+import java.util.Scanner; 
+public class Main { 
+ public static void main(String args[ ]) throws  Exception { 
+ Scanner s = new Scanner(System.in); 
+ int qid = s.nextInt(); 
+ s.nextLine(); 
+ String message = s.nextLine(); 
+ FileWriter fw = new FileWriter("message.txt");  fw.write(message); 
+ fw.close(); 
+ System.out.println("Message Written to File");  } 
+}
+
